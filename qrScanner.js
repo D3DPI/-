ada125e4c745559f23fe5d3633b922f396ab949c
D3DPI <97098888+D3DPI@users.noreply.github.com>
2024-06-21 18:29:43 +0800

    Update qrScanner.js

--- qrScanner.js
+++ qrScanner.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const videoElement = document.getElementById('video');
     const qrScanner = new QrScanner(videoElement, result => {
         if (result.includes('https://d3dpi.github.io/x/b.html')) {
             window.location.href = result;
